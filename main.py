ad5a4624707244c0801650768bdefc0d559ccee9
Julius Butkus <j.butkus.skouds@gmail.com>
2023-09-08 10:59:08 +0300

    priesai

--- main.py
+++ main.py
@@ -12,7 +12,30 @@ class Player:
     mana = 50
 
 class Enemies:
-    pass
+    def __init__(self, name, attack, hp, ):
+        self.name = name
+        self.attack = attack
+        self.hp = hp
+    
+    def goblin(self, name, attack, hp, ):
+        name = "Goblin"
+        attack = ["basic attack", "light punsh", "hevy attack", "critical hit"]
+        hp = 100
+        action = random.randint(1, 4)
+        for action in range(1, 4):
+            if action == 1:
+                attack[0] == 
+
+
+
+
+    def Raven():
+        
+    def Puppy():
+    
+    def dragon():
+
+
 
 class Magic:
     pass
