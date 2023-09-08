bfbc62ac78d0555789a7bc2f59fae5b20f2d5edd
Niazaris <giedrius954@gmail.com>
2023-09-08 09:50:00 +0300

    Main

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-##Zaidimas
+"""
+10 speliu
+4 enamies tipius
+5 stages
+stage 6 bosiukas"""
+
+import math
+import random
+
+class Player:
+    hp = 100
+    mana = 50
+
+class Enemies:
+    pass
+
+class Magic:
+    pass
+
+class World:
+    pass
